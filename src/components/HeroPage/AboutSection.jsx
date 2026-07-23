3d3d99df6bd7e86f89b27e9c515802041513653b
Lalit Sahu <lalit.sahu@iphtechnologies.com>
2026-07-23 18:09:59 +0530

    about section update

--- src/components/HeroPage/AboutSection.jsx
+++ src/components/HeroPage/AboutSection.jsx
@@ -3,7 +3,7 @@ import AboutImage from "./AboutImage";
 
 export default function AboutSection() {
   return (
-    <section id="about" className="bg-[#fff] py-12 sm:py-16 lg:py-20 overflow-x-clip">
+    <section id="about" className="bg-[#fff] py-12 sm:py-16 lg:py-20  overflow-x-clip">
       <div className="max-w-[1140px] mx-auto px-5">
 
       <div className="grid lg:grid-cols-[52%_48%] gap-6 lg:gap-4 items-center">
